9716fc6d2da5eb35ae740bb161723575458d02c6
JeppeSigaard <jeppe@smatmonkey.dk>
2015-06-10 14:26:10 +0200

    links på persongalleri

---
+++ archive.php
@@ -0,0 +1,11 @@
+<?php get_header(); ?>
+<section class="page">
+    <main>
+        <article>
+        </article>
+    </main>
+    <aside>
+        
+    </aside>
+</section>
+<?php get_footer(); ?>
--- section/person-gallery.php
+++ section/person-gallery.php
@@ -15,22 +15,29 @@ $person = new WP_Query(array(
 
 <section class="person-gallery" id="team">
     <ul class="person-list box-container">
-        <?php while ($person->have_posts()) : $person->the_post(); ?>
-            <li class="box person">
-                <a class="box-inner" href="<?php the_permalink(); ?>">
-                    <header>
-                        <?php $image_url = wp_get_attachment_image_src(get_post_meta(get_the_ID(),'profile_img',true),'front-people');?>
-                        <img src="<?php echo $image_url[0]; ?>">
-                    </header>
-                    <footer>
-                        <h3><?php echo get_post_meta(get_the_ID(),'titel',true); ?></h3>
-                        <h4><?php the_title(); ?></h4>
-                        <?php $subtitles = get_post_meta(get_the_ID(),'undertitel',true); foreach ($subtitles as $sub) : ?>
-                        <span><?php echo esc_textarea($sub); ?></span>
-                        <?php endforeach; ?>
-                    </footer>
-                </a>
-            </li>
+        <?php 
+        while ($person->have_posts()) : $person->the_post(); 
+        $telefon = str_replace(' ','',get_post_meta(get_the_ID(),'telefon',true));
+        $email = get_post_meta(get_the_ID(),'email',true);
+        ?>
+        <li class="box person">
+            <a class="box-inner" href="<?php the_permalink(); ?>"></a>
+            <header>
+                <?php $image_url = wp_get_attachment_image_src(get_post_meta(get_the_ID(),'profile_img',true),'front-people');?>
+                <img src="<?php echo $image_url[0]; ?>">
+                <div class="links">
+                    <a href="mailto:<?php echo $email ?>"><?php echo $email;?></a>
+                    <a href="tel:<?php echo $telefon ?>"><?php echo $telefon;?></a>
+                </div>
+            </header>
+            <footer>
+                <h3><?php echo get_post_meta(get_the_ID(),'titel',true); ?></h3>
+                <h4><?php the_title(); ?></h4>
+                <?php $subtitles = get_post_meta(get_the_ID(),'undertitel',true); foreach ($subtitles as $sub) : ?>
+                <span><?php echo esc_textarea($sub); ?></span>
+                <?php endforeach; ?>
+            </footer>
+        </li>
         <?php endwhile; ?>
     </ul>
 </section>

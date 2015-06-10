fefd79761015ef5c0094a84e3facb5790f77afae
JeppeSigaard <jeppe@smatmonkey.dk>
2015-06-10 13:02:23 +0200

    Småændringer, css persongalleri

--- page-templates/klippekort.php
+++ page-templates/klippekort.php
@@ -6,9 +6,19 @@ template name: Klippekort
 
 get_header();
 
+$client = get_post(86);
+
 ?>
 
 <section class="page">
+    <main>
+        <article>
+        <?php var_dump($client); ?>
+        </article>
+    </main>
+    <aside>
+        
+    </aside>
 </section>
 
 <?php get_footer(); ?>

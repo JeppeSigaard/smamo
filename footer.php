477f59658a974858dfc9138cbbed90fc411c8d67
JeppeSigaard <jeppe@smatmonkey.dk>
2015-09-03 15:43:42 +0200

    oprettet placeholder fixed footer

--- footer.php
+++ footer.php
@@ -1,6 +1,8 @@
 <?php $options = get_option('footer_options'); ?>
 <footer id="footer" class="footer">
-    
+    <div class="inner">
+        
+    </div>
 </footer>
 
 <?php wp_footer(); ?>    
--- js/modules/adjust-elem-heights.js
+++ js/modules/adjust-elem-heights.js
@@ -1,33 +1,13 @@
 var adjustElemHeights = function(){
-    
     $('.sectioned-header, .section-slider, .section-single_image').each(function(){
-    
-    
-    
-    
-        // If not mobile, set hero height to full height
-        if(!is_mobile() && $(window).width() > 768){
-
-            $(this).height($(window).height() - 24);
-        }
-
-        // Else do full height minus 50 and no resize jabber
-        else{
-             $(this).height($(window).height() - 84);
-        }
-        
+        $(this).height($(window).height() - 24);
     });
-
 }
-
 $(function(){
-    
     adjustElemHeights();
-    
-    $(window).resize(function(){
-    
-        adjustElemHeights();   
-    
-    });
-
+    if(!is_mobile()){
+        $(window).resize(function(){
+            adjustElemHeights();   
+        });
+    }
 });

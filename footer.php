f74969bf70a1268ada149fd93f35e03ab5f4ef4c
JeppeSigaard <jeppe@smatmonkey.dk>
2015-09-25 20:16:00 +0200

    modalboks til booking

--- footer.php
+++ footer.php
@@ -18,7 +18,6 @@
         </div>
     </div>
 </footer>
-
 <?php wp_footer(); ?>    
 </body>
 </html>
---
+++ modals/booking.php
@@ -0,0 +1,26 @@
+<div class="modal">
+    <div class="inner">
+        <form class="booking" action="" method="post">
+            <div>
+                <h3>Fedt mand!</h3>
+                <p>Vi vil vildt gerne mødes med dig!<br/>
+                Hvor skal vi fange dig for at aftale nærmere?</p>
+            </div>
+            <div>
+                <input name="name" type="text" required>
+                <label for="name">Navn:</label>
+            </div>
+            <div>
+                <input name="telefon" type="text" required>
+                <label for="telefon">Tlf:</label>
+            </div>
+            <div>
+                <input name="email" type="email" required>
+                <label for="email">E-dresse:</label>
+            </div>
+            <div class="buttons center">
+                <input type="submit" class="button black" value="book nu">
+            </div>
+        </form>
+    </div>
+</div>

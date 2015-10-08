28dba1ba49671d929f5005589e9948a034844752
JeppeSigaard <jeppe@smatmonkey.dk>
2015-10-08 10:35:35 +0200

    opdateret videobaggrund

--- section/subsection/section-videobg.php
+++ section/subsection/section-videobg.php
@@ -9,11 +9,10 @@
                 <?php endif; ?>
             </div>
         </div>
-        <?php if ( !wp_is_mobile() ) : ?>
-        <?php echo wp_oembed_get(get_post_meta(get_the_ID(),'video_bg',true).'&modestbranding=1&enablejsapi=1&autplay=1&rel=0',array('width' => '2', 'height' => '2'));?>
-        <?php else : ?>
+        <?php if ( !wp_is_mobile() ) {
+            echo wp_oembed_get(get_post_meta(get_the_ID(),'video_bg',true).'&modestbranding=1&enablejsapi=1&autplay=1&rel=0',array('width' => '2', 'height' => '2'));
+        } ?>
         <?php $video_fb = wp_get_attachment_image_src( get_post_meta(get_the_ID(),'video_bg_fallback',true), 'widescreen' );  ?>
         <div class="video-bg-fallback" data-bg="<?php if (is_array($video_fb)) { echo $video_fb[0]; } ?>"></div>
-        <?php endif; ?>
     </div>
 </section>
